45135e2ee25a7bdead869fc69ce8cdebd778c22e
NgocQuin <tranngocquyen262dc@gmail.com>
2024-05-31 05:14:26 +0700

    xoa xung dot

--- Frontend/src/services/imgLoadFirebase.js
+++ Frontend/src/services/imgLoadFirebase.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-// import { initializeApp } from 'firebase/app';
-// import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
-// import config from '../config/firebaseConfig';
-// //Initialize a firebase application
-// initializeApp(config.firebaseConfig);
-// let uploadImg = async (fileData) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             console.log('fileData from imgLoad: ', fileData.path);
-//             if (!fileData.buffer) {
-//                 console.log('File buffer is empty: ', fileData.buffer);
-//                 resolve();
-//             }
-//             let fileBuffer = Buffer.from(fileData.buffer, 'binary');
-//             let fileStream = new Readable();
-//             fileStream.push(fileBuffer);
-//             fileStream.push(null);
-//             let metadata = {
-//                 metadata: {
-//                     contentType: fileData.mimetype,
-//                 },
-//             };
-//             let uploadResponse = await bucket.upload(fileStream, {
-//                 destination: fileData.originalname,
-//                 metadata: metadata,
-//             });
-//             let url = uploadResponse[0].metadata.mediaLink;
-//             resolve(url);
-//         } catch (error) {
-//             reject(error);
-//         }
-//     });
-// };
-// let storage = getStorage();
-// let getUrlFirebase = async (fileData) => {
-//     let dateTime = giveCurrentDateTime();
-//     let storageRef = ref(storage, `images/${fileData.originalname + dateTime}`);
-//     let metadata = {
-//         contentType: fileData.mimetype,
-//     };
-//     const snapshot = await uploadBytesResumable(storageRef, fileData.buffer, metadata);
-//     let downloadURL = await getDownloadURL(snapshot.ref);
-//     resolve(downloadURL);
-// };
-// let giveCurrentDateTime = () => {
-//     let today = new Date();
-//     let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-//     let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-//     let dateTime = date + ' ' + time;
-//     return dateTime;
-// };
-// module.exports = {
-//     uploadImg: uploadImg,
-//     getUrlFirebase: getUrlFirebase,
-// };
-
 const { initializeApp } = require('firebase/app');
 const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
 const { Readable } = require('stream');
@@ -63,17 +6,6 @@ const config = require('../config/firebaseConfig').firebaseConfig;
 // Initialize Firebase
 const firebaseApp = initializeApp(config);
 const storage = getStorage(firebaseApp);
-
-=======
-const { initializeApp } = require('firebase/app');
-const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
-const { Readable } = require('stream');
-const config = require('../config/firebaseConfig').firebaseConfig;
-
-// Initialize Firebase
-const firebaseApp = initializeApp(config);
-const storage = getStorage(firebaseApp);
->>>>>>> branchTestQ
 const uploadImg = async (fileData) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -82,19 +14,11 @@ const uploadImg = async (fileData) => {
                 resolve(null);
                 return;
             }
-<<<<<<< HEAD
-
-=======
->>>>>>> branchTestQ
             const dateTime = giveCurrentDateTime();
             const storageRef = ref(storage, `images/${fileData.originalname}-${dateTime}`);
             const metadata = {
                 contentType: fileData.mimetype,
             };
-<<<<<<< HEAD
-
-=======
->>>>>>> branchTestQ
             const snapshot = await uploadBytesResumable(storageRef, fileData.buffer, metadata);
             const downloadURL = await getDownloadURL(snapshot.ref);
 
@@ -104,10 +28,7 @@ const uploadImg = async (fileData) => {
         }
     });
 };
-<<<<<<< HEAD
 
-=======
->>>>>>> branchTestQ
 const giveCurrentDateTime = () => {
     const today = new Date();
     const date = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(
